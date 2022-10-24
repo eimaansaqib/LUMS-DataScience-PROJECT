531c1081a0032301b8b6d3ddb92945059a03b0f3
Muhammad Hamza <m.waqas.hamza@gmail.com>
2022-10-24 13:11:03 +0500

    add error handling

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -1,6 +1,31 @@
 from json import dump
+from time import sleep
 import pandas as pd
 from requests import get
+
+
+def get_data(game_title, platform):
+    """Get data from metacritic
+
+    :param game_title: title of the game
+    :type title: str
+    :param platform: platform of the game
+    :type platform: str
+    :return: data from metacrictic
+    :rtype: dict
+    """
+
+    try:
+        res_json = get(ENDPOINT, params={
+            "game_title": game_title, "platform": platform}).json()
+
+        res_json["name"] = game_title
+        return res_json
+
+    except ConnectionError:
+        sleep(1)
+        return get_data(title, platform)
+
 
 ENDPOINT = "https://metacritic-api.azurewebsites.net/"
 PLATFORM = "playstation-4"
@@ -10,9 +35,7 @@ responses = []
 
 for idx, title in enumerate(data["title"]):
     print(idx, title)
-    responses.append(get(ENDPOINT,
-                         params={"game_title": title, "platform": PLATFORM}
-                         ).json())
+    responses.append(get_data(title, PLATFORM))
 
 with open("ps4.json", "w", encoding="UTF-8") as outfile:
     dump(responses, outfile)
